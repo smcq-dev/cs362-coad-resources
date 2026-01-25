74829f53b9c8314bc007c776380045b751ae1c51
Sean McQuillan <seanrmcquillan@gmail.com>
2026-01-24 21:15:37 -0800

    Description test added.

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe Ticket, type: :model do
     expect(ticket).to respond_to(:name)
   end
 
+   it 'has a description attribute' do
+    expect(ticket).to respond_to(:description)
+  end
+
 
 end
