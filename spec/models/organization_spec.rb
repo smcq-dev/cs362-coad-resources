d5efedeed633b46b46e9fcad0cafd21fce84a4cb
Sean McQuillan <seanrmcquillan@gmail.com>
2026-01-25 14:21:04 -0800

    Organization should validate presence of primary_name.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -96,5 +96,7 @@ RSpec.describe Organization, type: :model do
   it { should validate_presence_of(:phone) }
 
   it { should validate_presence_of(:status) }
+
+  it { should validate_presence_of(:primary_name) }
  
 end
