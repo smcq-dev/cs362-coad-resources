408ff90e3a43806b607e1f886e87f3abf855bcd8
Sean McQuillan <seanrmcquillan@gmail.com>
2026-01-24 21:12:30 -0800

    Name test attribute added.

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+  let(:resource) { ResourceCategory.new }
 
+  it 'has a name attribute' do
+    expect(resource).to respond_to(:name)
+  end
 end
