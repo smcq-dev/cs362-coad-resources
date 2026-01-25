06972ea42ce01f25255bee73beda50a311acfb7c
Sean McQuillan <seanrmcquillan@gmail.com>
2026-01-24 21:34:28 -0800

    Organizations should have and belong to many resources.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -86,5 +86,7 @@ RSpec.describe Organization, type: :model do
   it { should have_many(:users) }
 
   it { should have_many(:tickets) }
+
+  it { should have_and_belong_to_many(:resource_categories) }
  
 end
