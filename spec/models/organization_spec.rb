baf18dc17ac6a6e73635ab4f9588a3dc451e45fd
Sean McQuillan <seanrmcquillan@gmail.com>
2026-01-24 20:49:47 -0800

    Agreement_two test added.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-
+  let(:organization) { Organization.new }
+  
   it 'has a agreement_one attribute' do
-  organization = Organization.new
-  expect(organization).to respond_to(:agreement_one)
+    expect(organization).to respond_to(:agreement_one)
   end
 
+  it 'has a agreement_two attribute' do
+    expect(organization).to respond_to(:agreement_two)
+  end
+
+  
+ 
 end
