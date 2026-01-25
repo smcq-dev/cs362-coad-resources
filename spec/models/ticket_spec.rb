2abcdbdf07e6580cee4a5a64693d4239b16f7911
Sean McQuillan <seanrmcquillan@gmail.com>
2026-01-24 21:14:30 -0800

    Name test added.

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
+  let (:ticket) { Ticket.new }
+
+   it 'has a name attribute' do
+    expect(ticket).to respond_to(:name)
+  end
+
 
 end
