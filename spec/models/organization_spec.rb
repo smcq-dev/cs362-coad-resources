19e7808ef030e9ad70398dc698552d899293d28c
Sean McQuillan <seanrmcquillan@gmail.com>
2026-01-25 14:19:11 -0800

    Organization should validate presence of phone.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -92,5 +92,7 @@ RSpec.describe Organization, type: :model do
   it { should validate_presence_of(:name) }
 
   it { should validate_presence_of(:email) }
+
+   it { should validate_presence_of(:phone) }
  
 end
