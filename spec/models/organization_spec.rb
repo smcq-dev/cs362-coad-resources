750f4cb6364bb1dae3593ea49e2ae6f343261e97
Sean McQuillan <seanrmcquillan@gmail.com>
2026-01-25 14:32:46 -0800

    Organization should validate the REGEX pattern of email.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -104,5 +104,9 @@ RSpec.describe Organization, type: :model do
   it { should validate_presence_of(:secondary_phone) }
 
   it { should validate_length_of(:email).is_at_least(1).is_at_most(255) }
+
+  it { should allow_value('test@email.com').for(:email) }
+
+  it { should_not allow_value('test-email').for(:email) }
  
 end
