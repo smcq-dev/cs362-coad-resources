ef052cdc664e0a5ee556537b43692eecd46f1ef6
Sean McQuillan <seanrmcquillan@gmail.com>
2026-01-25 14:20:08 -0800

    Organization should validate presence of status.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -93,6 +93,8 @@ RSpec.describe Organization, type: :model do
 
   it { should validate_presence_of(:email) }
 
-   it { should validate_presence_of(:phone) }
+  it { should validate_presence_of(:phone) }
+
+  it { should validate_presence_of(:status) }
  
 end
