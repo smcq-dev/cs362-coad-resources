3c0f742214747fad60ad26d6babfe10e55348b8f
Sean McQuillan <seanrmcquillan@gmail.com>
2026-01-24 20:51:06 -0800

    Agreement_three test added.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
   let(:organization) { Organization.new }
-  
+
   it 'has a agreement_one attribute' do
     expect(organization).to respond_to(:agreement_one)
   end
 
   it 'has a agreement_two attribute' do
     expect(organization).to respond_to(:agreement_two)
+  end
+
+  it 'has a agreement_three attribute' do
+    expect(organization).to respond_to(:agreement_three)
   end
 
   
