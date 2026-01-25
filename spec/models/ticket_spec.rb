538545832e043400e9e95abe3d5dcf6d9f965612
Sean McQuillan <seanrmcquillan@gmail.com>
2026-01-25 14:56:27 -0800

    Ticket should validate length of description.

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -39,4 +39,6 @@ RSpec.describe Ticket, type: :model do
 
   it { should validate_length_of(:name).is_at_least(1).is_at_most(255) }
 
+  it { should validate_length_of(:description).is_at_most(1020) }
+
 end
