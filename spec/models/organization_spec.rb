0de1e89bce0fd93fef45df2ad13aab9270049104
Sean McQuillan <seanrmcquillan@gmail.com>
2026-01-25 14:17:10 -0800

    Organization should validate presence of name.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -88,5 +88,7 @@ RSpec.describe Organization, type: :model do
   it { should have_many(:tickets) }
 
   it { should have_and_belong_to_many(:resource_categories) }
+
+  it { should validate_presence_of(:name) }
  
 end
