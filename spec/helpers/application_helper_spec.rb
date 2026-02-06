2e0a0fac88e563daf09184d17ba53cf3b115dacb
Nick Nyburg <nick@Mac.lan>
2026-02-05 22:55:01 -0800

    Added application_helper title tests

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-
+  describe "full_title" do
+    it "returns base title when page title is empty by default" do 
+      expect(full_title).to eq("Disaster Resource Network")
+    end
+    it "returns base title when page title is an empty string" do
+      expect(full_title("")).to eq("Disaster Resource Network")
+    end
+    it "returns full title when page title is provided" do
+      expect(full_title("Home")).to eq("Home | Disaster Resource Network")
+    end
+  end
 end
