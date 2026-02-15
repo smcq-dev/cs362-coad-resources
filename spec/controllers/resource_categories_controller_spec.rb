c5ec2ace5e3f7e64b7cf8538813bb9606942c372
Sean McQuillan <seanrmcquillan@gmail.com>
2026-02-14 20:10:44 -0800

    Resource category controller update method tested.

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -12,6 +12,10 @@ RSpec.describe ResourceCategoriesController, type: :controller do
     it {
       expect(get(:new)).to redirect_to new_user_session_path
     }
+
+    it {
+      expect(patch(:update, params: {id: 1, resource_category: FactoryBot.attributes_for(:resource_category)})).to redirect_to new_user_session_path
+    }
   end
 
   describe 'as a logged in user' do
@@ -25,11 +29,15 @@ RSpec.describe ResourceCategoriesController, type: :controller do
     it {
       expect(get(:new)).to redirect_to dashboard_path
     }
+
+    it {
+      expect(patch(:update, params: {id: 1, resource_category: FactoryBot.attributes_for(:resource_category)})).to redirect_to dashboard_path
+    }
   end
 
   describe 'as an admin user' do 
     let(:user) { create(:user, :admin)}
-    let(:region) { create(:resources_category)}
+    let(:resource_category) { create(:resource_category)}
     before(:each) {sign_in user}
 
     it {
@@ -38,6 +46,10 @@ RSpec.describe ResourceCategoriesController, type: :controller do
 
     it {
       expect(get(:new)).to be_successful
+    }
+
+    it {
+      expect(patch(:update, params: {id: resource_category.id, resource_category: FactoryBot.attributes_for(:resource_category)})).to redirect_to resource_category_path(resource_category) 
     }
   end
 end
