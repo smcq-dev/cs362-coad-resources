5bd9ae6ca3bcbddb1dcc035952503a2bdb06b3c9
Dylan De Forrest <deforred@oregonstate.edu>
2026-02-15 23:41:24 -0800

    organization logged in user tests

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -33,6 +33,43 @@ RSpec.describe OrganizationsController, type: :controller do
       expect(response).to redirect_to new_user_session_path
 
     }
-
   end 
+
+  describe 'as a logged in user' do 
+    let(:user) {create(:user)}
+    before(:each) {sign_in user}
+
+    it {
+      expect(get(:index)).to be_successful
+    }
+
+    it {
+      expect(get(:new)).to be_successful
+    }
+
+    # it {
+    #   expect(get(:edit)).to be_successful
+    # }
+
+    it {
+      expect(put(:update, params: {id: 1})).to redirect_to dashboard_path
+    }
+
+    it {
+      expect(post(:approve, params: {id: 1})).to redirect_to dashboard_path
+    }
+
+    it {
+      expect(post(:reject, params: {id: 1})).to redirect_to dashboard_path
+    }
+
+    it {
+      create(:user, role: :admin)
+      post(:create, params: {organization: FactoryBot.attributes_for(:organization)})
+      expect(response).to redirect_to organization_application_submitted_path
+
+    }
+  end 
+
+
 end
