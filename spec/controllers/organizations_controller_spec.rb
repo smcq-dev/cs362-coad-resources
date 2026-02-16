28147e1a1fe5baba7627c252a52a3164b664d283
Dylan De Forrest <deforred@oregonstate.edu>
2026-02-15 23:25:57 -0800

    organization logged put user test complete

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
+  describe 'as logged out user' do 
+    let(:user) {create(:user)}
 
+    it {
+      expect(get(:index)).to redirect_to new_user_session_path
+    }
+
+    it {
+      expect(get(:new)).to redirect_to new_user_session_path
+    }
+
+    # it {
+    #   expect(get(:edit)).to be_successful
+    # }
+
+    it {
+      expect(put(:update, params: {id: 1})).to redirect_to new_user_session_path
+    }
+
+    it {
+      expect(post(:approve, params: {id: 1})).to redirect_to new_user_session_path
+    }
+
+    it {
+      expect(post(:reject, params: {id: 1})).to redirect_to new_user_session_path
+    }
+
+    it {
+      post(:create, params: {organization: FactoryBot.attributes_for(:organization)})
+      expect(response).to redirect_to new_user_session_path
+
+    }
+
+  end 
 end
