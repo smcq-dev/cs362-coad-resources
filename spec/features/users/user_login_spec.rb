0e37d35b0531851db12452d789127f51e9ed21f0
Dylan De Forrest <deforred@oregonstate.edu>
2026-03-13 20:01:45 -0700

    user end to end tests

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
-
+ 
 RSpec.describe 'Logging in', type: :feature do
-
+  before do
+    @user = create(:user)
+  end
+ 
+  it "can log in with valid credentials" do
+ 
+    visit login_path
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: "password123"
+    click_on "Sign in"
+ 
+    expect(current_path).to eq dashboard_path
+ 
+  end
 end
+ 
--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
-
+ 
 RSpec.describe 'User registration', type: :feature do
-
+ 
+  it "can register a new user" do
+ 
+    visit signup_path
+    fill_in "Email", with: "newuser@example.com"
+    fill_in "Password", with: "password123"
+    fill_in "Password confirmation", with: "password123"
+    click_button "Sign up"
+ 
+    expect(current_path).to eq root_path
+ 
+  end
 end
+ 
